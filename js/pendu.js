b5d10dc89b6a12713ca5096c7f16f077ff9f6285
Jkail <boutaultbenoit@yahoo.fr>
2019-07-10 19:28:39 +0200

    test

--- js/pendu.js
+++ js/pendu.js
@@ -1,5 +1,5 @@
 var solution = document.getElementById('solution').value;
-console.log(solution);
+console.log(solution); 
 /*verifier que au moins 2 lettres, pas de chiffre et de caracètres speciaux
 convertir le mot en majuscule
 si tout est bon, sauvegarde dans une variable le mot à trouver
