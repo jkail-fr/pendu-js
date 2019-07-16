a69f5822ecfb0ca4f97a8ae8533610d7551a0481
Jkail <boutaultbenoit@yahoo.fr>
2019-07-16 21:24:43 +0200

    Start JS validation of inputed word

--- js/pendu.js
+++ js/pendu.js
@@ -1,5 +1,9 @@
 var solution = document.getElementById('solution').value;
-console.log(solution); 
-/*verifier que au moins 2 lettres, pas de chiffre et de caracètres speciaux
+console.log(solution); /* a virer*/
+/* verifier que au moins 2 lettres, pas de chiffre et de caracètres speciaux
 convertir le mot en majuscule
-si tout est bon, sauvegarde dans une variable le mot à trouver
+si tout est bon, sauvegarde dans une variable le mot à trouver */
+
+
+var validation = document.getElementById('validation');
+
