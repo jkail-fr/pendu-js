2430c10e01403ad5230ba8a08a91039223519caa
Jkail <boutaultbenoit@yahoo.fr>
2019-07-23 23:16:30 +0200

    end of coding session N°3

--- js/pendu.js
+++ js/pendu.js
@@ -1,19 +1,33 @@
-/**Global**/
-document.getElementById('enigme').style.display = "block";
-/*document.getElementById('chercher').style.display = "none";*/
+/**Global et affichage**/
+var nombreTentatives = 12;
+/* changer ce chiffre pour modifier le nombre de tentatives... attention les dessins ne correspondront plus*/
+var tentatives = nombreTentatives;
 
+function affichageTentatives(quantiteTentatives) {
+    document.querySelector('#statut p span.tentatives').innerHTML = quantiteTentatives;
+}
+affichageTentatives(tentatives);
+
+function reinitialisation() {
+    document.getElementById('enigme').style.display = "block";
+    /*document.getElementById('chercher').style.display = "none";*/
+    document.getElementById('solution').value = null;
+    tentatives = nombreTentatives;
+    affichageTentatives(nombreTentatives);
+}
+reinitialisation();
+/* on lance le jeu*/
 
 /***Reset affichage ***/
+/*On gère le clic sur reset*/
 var reset = document.getElementById('reset');
-var resetGame = function() {
-    document.getElementById('enigme').style.display = "block";
-    document.getElementById('chercher').style.display = "none";
-    document.getElementById('solution').value = null;
-}
+var resetGame = function () {
+    reinitialisation();
+};
 reset.addEventListener('click', resetGame);
 
 
-/*Création du mot*/
+/**Création du mot**/
 var boutonValidation = document.getElementById('validation');
 
 function verification() {
@@ -34,10 +48,40 @@ function verification() {
 var textValidation = function () {
     var motATrouver = verification();
     console.log(motATrouver);
-}
+};
 boutonValidation.addEventListener('click', textValidation);
 
-/* Deviner le mot */
-var tentatives = 12;
-console.log("il reste " + tentatives + ". ");
-/** TO DO : bouton diminue nombre tentative; afficher nombre tentative; chaque tentative affiche un bonhomme différent (switch case) **/
+/** Deviner le mot **/
+
+/*Clic sur le bouton deviner*/
+var deviner = document.getElementById('deviner');
+var nouvelleLettre = function ()
+{
+    tentatives = tentatives - 1;
+    document.querySelector('#statut p span.tentatives').innerHTML = tentatives;
+    if (tentatives > 0)
+    {
+        switch (tentatives)
+        {
+            case 11:
+                console.log('toto11');
+                break;
+            case 10:
+                console.log('totot10');
+                break;
+        }
+    }
+
+    else if (tentatives <= 0)
+    {
+        alert(' echec du jeu');
+        reinitialisation();
+    }
+};
+deviner.addEventListener('click', nouvelleLettre);
+
+
+/** TO DO :  finir le switch, limiter le champs de devinette à 1 lettre - bloquer les chiffres - convertir mo a trouver en array - parser l'array sur chatque tentattive et gerer l'affichage
+ *
+ * **/
+
