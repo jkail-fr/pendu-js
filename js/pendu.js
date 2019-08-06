029e1decf7d9c62e07ee71166836c748d664a613
Jkail <boutaultbenoit@yahoo.fr>
2019-08-06 23:04:01 +0200

    end of coding session N°5

--- js/pendu.js
+++ js/pendu.js
@@ -13,11 +13,12 @@ affichageTentatives(tentatives);
 function reinitialisation()
 	{
 		document.getElementById('enigme').style.display = "block";
-		/*document.getElementById('chercher').style.display = "none";*/
+		document.getElementById('chercher').style.display = "none";
 		document.getElementById('solution').value = null;
 		document.getElementById('tentative').value = null;
 		document.querySelector('#statut div#pendu').innerHTML = null;
 		document.querySelector('#proposition span').innerHTML = null;
+		document.querySelector('#mot-mystere').innerHTML = null;
 		tentatives = nombreTentatives;
 		affichageTentatives(nombreTentatives);
 	}
@@ -38,6 +39,7 @@ reset.addEventListener('click', function ()
 var boutonValidation = document.getElementById('validation');
 
 var resultat = null;
+
 function verification()
 	{
 		var solution = document.getElementById('solution').value;
@@ -72,54 +74,85 @@ boutonValidation.addEventListener('click', function ()
 var deviner = document.getElementById('deviner');
 
 function nouvelleLettre()
-	{ 	var tenteTaChance = document.getElementById('tentative').value;
-		document.querySelector('#proposition span').innerHTML += tenteTaChance +' ';
-		console.log(resultat);
-		tentatives = tentatives - 1;
-		document.querySelector('#statut p span.tentatives').innerHTML = tentatives;
-		if (tentatives > 0)
+	{
+		var tenteTaChance = document.getElementById('tentative').value;
+
+		//https://stackoverflow.com/questions/9690429/get-multiple-character-positions-in-array
+		var getAllIndexesOf = function (str, toFind)
 			{
-				switch (tentatives)
-				{
-					case 10:
-						document.querySelector('#statut div#pendu').innerHTML = "<img src='./pendu-image/pendu1.png'/>";
-						break;
-					case 9:
-						document.querySelector('#statut div#pendu').innerHTML = "<img src='./pendu-image/pendu2.png'/>";
-						break;
-					case 8:
-						document.querySelector('#statut div#pendu').innerHTML = "<img src='./pendu-image/pendu3.png'/>";
-						break;
-					case 7:
-						document.querySelector('#statut div#pendu').innerHTML = "<img src='./pendu-image/pendu4.png'/>";
-						break;
-					case 6:
-						document.querySelector('#statut div#pendu').innerHTML = "<img src='./pendu-image/pendu5.png'/>";
-						break;
-					case 5:
-						document.querySelector('#statut div#pendu').innerHTML = "<img src='./pendu-image/pendu6.png'/>";
-						break;
-					case 4:
-						document.querySelector('#statut div#pendu').innerHTML = "<img src='./pendu-image/pendu7.png'/>";
-						break;
-					case 3:
-						document.querySelector('#statut div#pendu').innerHTML = "<img src='./pendu-image/pendu8.png'/>";
-						break;
-					case 2:
-						document.querySelector('#statut div#pendu').innerHTML = "<img src='./pendu-image/pendu9.png'/>";
-						break;
-					case 1:
-						document.querySelector('#statut div#pendu').innerHTML = "<img src='./pendu-image/pendu10.png'/>";
-						break;
-				}
+				var all = [];
+				for (var i = 0; i < str.length; i++)
+					{
+						if (toFind.indexOf(str[i]) >= 0)
+							{
+								all.push(i);
+							}
+					}
+				return all;
+			};
+
+		var positions = getAllIndexesOf(resultat, tenteTaChance);
+		//console.log(positions);
+		if (positions.length < 1 || positions == undefined)
+			{
+				document.querySelector('#proposition span').innerHTML += tenteTaChance + ' ';
+				tentatives = tentatives - 1;
+				document.querySelector('#statut p span.tentatives').innerHTML = tentatives;
+				if (tentatives > 0)
+					{
+						switch (tentatives)
+						{
+							case 10:
+								document.querySelector('#statut div#pendu').innerHTML = "<img src='./pendu-image/pendu1.png' width='75%'/>";
+								break;
+							case 9:
+								document.querySelector('#statut div#pendu').innerHTML = "<img src='./pendu-image/pendu2.png' width='75%'/>";
+								break;
+							case 8:
+								document.querySelector('#statut div#pendu').innerHTML = "<img src='./pendu-image/pendu3.png' width='75%'/>";
+								break;
+							case 7:
+								document.querySelector('#statut div#pendu').innerHTML = "<img src='./pendu-image/pendu4.png' width='75%'/>";
+								break;
+							case 6:
+								document.querySelector('#statut div#pendu').innerHTML = "<img src='./pendu-image/pendu5.png' width='75%'/>";
+								break;
+							case 5:
+								document.querySelector('#statut div#pendu').innerHTML = "<img src='./pendu-image/pendu6.png' width='75%'/>";
+								break;
+							case 4:
+								document.querySelector('#statut div#pendu').innerHTML = "<img src='./pendu-image/pendu7.png' width='75%'/>";
+								break;
+							case 3:
+								document.querySelector('#statut div#pendu').innerHTML = "<img src='./pendu-image/pendu8.png' width='75%'/>";
+								break;
+							case 2:
+								document.querySelector('#statut div#pendu').innerHTML = "<img src='./pendu-image/pendu9.png' width='75%'/>";
+								break;
+							case 1:
+								document.querySelector('#statut div#pendu').innerHTML = "<img src='./pendu-image/pendu10.png' width='75%'/>";
+								break;
+						}
+					}
+
+				else if (tentatives <= 0)
+					{
+						document.querySelector('#statut div#pendu').innerHTML = "<img src='./pendu-image/pendu11.png' width='75%'/>";
+						alert('échec du jeu');
+						reinitialisation();
+					}
+			}
+		else if (positions.length >= 0)
+			{
+				for (i = 0 ; i < positions.length; i++)
+					{	//il faut utiliser la conversion unicode du chiffre pur l'utiliser dans le query selector  -> https://stackoverflow.com/questions/20306204/using-queryselector-with-ids-that-are-numbers
+						var cible = '#\\3' + positions[i];
+						document.querySelector('#mot-mystere ' +cible).innerText = tenteTaChance;
+					}
+
 			}
 
-		else if (tentatives <= 0)
-			{
-				document.querySelector('#statut div#pendu').innerHTML = "<img src='./pendu-image/pendu11.png'/>";
-				alert(' echec du jeu');
-				reinitialisation();
-			}
+
 	}
 
 deviner.addEventListener('click', function ()
@@ -128,10 +161,9 @@ deviner.addEventListener('click', function ()
 });
 
 
-/** TO DO :   * parser l'array sur chatque tentattive et gerer l'affichage
- * styler le bordel-
- * limiter le champs de devinette à 1 lettre - bloquer les chiffres -
- * verifier si en js on peut empecher le navigateur d'enregistrer la saisie automatique pour le mot a fairte deviner
- *
+/** TO DO :
+ * limiter le champs de devinette à 1 lettre - bloquer les chiffres et convertir les accents, tout mettre en majuscule,
+ * prevoir alert fin du jeu + reinitialisation. (ou pas)-
+ *ajouter touchstart en envent en plus du clic ? (pour gestion tablette)
  * **/
 
