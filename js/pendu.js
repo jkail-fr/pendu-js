ae2c790a293b076daadb7772b83bcdabe1706524
Jkail <boutaultbenoit@yahoo.fr>
2019-08-30 22:46:40 +0200

    end of coding session N°5

--- js/pendu.js
+++ js/pendu.js
@@ -35,6 +35,23 @@ reset.addEventListener('click', function ()
 	reinitialisation();
 });
 
+// vérifier s'il n'y a aps de caractère spéciaux ou de chiffres)
+var forbidden = ["é", "è", "ê", "î", "ô", "â", "à", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+
+function checkForbidden(value)
+	{
+		for (var compteur = 0; compteur <= forbidden.length; compteur++)
+			{
+				var resultatCheck = value.includes(forbidden[compteur]);
+				if (resultatCheck === true)
+					{
+						var erreurDetectee = resultatCheck;
+					}
+				return erreurDetectee
+			}
+
+	}
+
 /**Création du mot**/
 var boutonValidation = document.getElementById('validation');
 
@@ -43,22 +60,33 @@ var resultat = null;
 function verification()
 	{
 		var solution = document.getElementById('solution').value;
+		solution = solution.toUpperCase();
 		var taille = solution.length;
-		if (taille > 2)
+		var verificationInput = checkForbidden(solution);
+		console.log(solution);
+		if (verificationInput === true)
 			{
-				/* TODO verifier que au moins 2 lettres, pas de chiffre et de caracètres speciaux convertir le mot en majuscule si tout est bon, sauvegarde dans une variable le mot à trouver */
-				document.getElementById('enigme').style.display = "none";
-				document.getElementById('chercher').style.display = "block";
-				var arraySplit = Array.from(solution);
-				for (var i = 0; i < arraySplit.length; i++)
-					{
-						document.querySelector('#mot-mystere').innerHTML += '<span id="' + i + '">_ </span>';
-					}
-				return arraySplit;
+				reinitialisation();
+				alert('Il ne faut pas de chiffres ou de caractères spéciaux');
+
 			}
 		else
 			{
-				alert("Votre mot est trop court, il faut au moins 3 caractères");
+				if (taille > 2)
+					{
+						document.getElementById('enigme').style.display = "none";
+						document.getElementById('chercher').style.display = "block";
+						var arraySplit = Array.from(solution);
+						for (var i = 0; i < arraySplit.length; i++)
+							{
+								document.querySelector('#mot-mystere').innerHTML += '<span id="' + i + '">_ </span>';
+							}
+						return arraySplit;
+					}
+				else
+					{
+						alert("Votre mot est trop court, il faut au moins 3 caractères");
+					}
 			}
 	}
 
@@ -76,84 +104,100 @@ var deviner = document.getElementById('deviner');
 function nouvelleLettre()
 	{
 		var tenteTaChance = document.getElementById('tentative').value;
+		tenteTaChance = tenteTaChance.toUpperCase();
 
-		//https://stackoverflow.com/questions/9690429/get-multiple-character-positions-in-array
-		var getAllIndexesOf = function (str, toFind)
+		verificationInput = checkForbidden(tenteTaChance);
+		if (verificationInput === true)
 			{
-				var all = [];
-				for (var i = 0; i < str.length; i++)
+				reinitialisation();
+				alert('Il ne faut pas de chiffres ou de caractères spéciaux');
+			}
+		else
+			{
+				if (tenteTaChance.length != 1)
 					{
-						if (toFind.indexOf(str[i]) >= 0)
+						alert('Veuillez ne poster qu\'une lettre');
+					}
+				else
+					{
+						//https://stackoverflow.com/questions/9690429/get-multiple-character-positions-in-array
+						var getAllIndexesOf = function (str, toFind)
 							{
-								all.push(i);
+								var all = [];
+								for (var i = 0; i < str.length; i++)
+									{
+										if (toFind.indexOf(str[i]) >= 0)
+											{
+												all.push(i);
+											}
+									}
+								return all;
+							};
+
+						var positions = getAllIndexesOf(resultat, tenteTaChance);
+						if (positions.length < 1 || positions == undefined)
+							{
+								document.querySelector('#proposition span').innerHTML += tenteTaChance + ' ';
+								tentatives = tentatives - 1;
+								document.querySelector('#statut p span.tentatives').innerHTML = tentatives;
+								if (tentatives > 0)
+									{
+										switch (tentatives)
+										{
+											case 10:
+												document.querySelector('#statut div#pendu').innerHTML = "<img src='./pendu-image/pendu1.png' width='75%'/>";
+												break;
+											case 9:
+												document.querySelector('#statut div#pendu').innerHTML = "<img src='./pendu-image/pendu2.png' width='75%'/>";
+												break;
+											case 8:
+												document.querySelector('#statut div#pendu').innerHTML = "<img src='./pendu-image/pendu3.png' width='75%'/>";
+												break;
+											case 7:
+												document.querySelector('#statut div#pendu').innerHTML = "<img src='./pendu-image/pendu4.png' width='75%'/>";
+												break;
+											case 6:
+												document.querySelector('#statut div#pendu').innerHTML = "<img src='./pendu-image/pendu5.png' width='75%'/>";
+												break;
+											case 5:
+												document.querySelector('#statut div#pendu').innerHTML = "<img src='./pendu-image/pendu6.png' width='75%'/>";
+												break;
+											case 4:
+												document.querySelector('#statut div#pendu').innerHTML = "<img src='./pendu-image/pendu7.png' width='75%'/>";
+												break;
+											case 3:
+												document.querySelector('#statut div#pendu').innerHTML = "<img src='./pendu-image/pendu8.png' width='75%'/>";
+												break;
+											case 2:
+												document.querySelector('#statut div#pendu').innerHTML = "<img src='./pendu-image/pendu9.png' width='75%'/>";
+												break;
+											case 1:
+												document.querySelector('#statut div#pendu').innerHTML = "<img src='./pendu-image/pendu10.png' width='75%'/>";
+												break;
+										}
+									}
+
+								else if (tentatives <= 0)
+									{
+										document.querySelector('#statut div#pendu').innerHTML = "<img src='./pendu-image/pendu11.png' width='75%'/>";
+										alert('échec du jeu');
+										reinitialisation();
+									}
+							}
+						else if (positions.length >= 0)
+							{
+								for (i = 0; i < positions.length; i++)
+									{	//il faut utiliser la conversion unicode du chiffre pur l'utiliser dans le query selector  -> https://stackoverflow.com/questions/20306204/using-queryselector-with-ids-that-are-numbers
+										var cible = '#\\3' + positions[i];
+										document.querySelector('#mot-mystere ' + cible).innerText = tenteTaChance;
+									}
+
 							}
 					}
-				return all;
-			};
-
-		var positions = getAllIndexesOf(resultat, tenteTaChance);
-		//console.log(positions);
-		if (positions.length < 1 || positions == undefined)
-			{
-				document.querySelector('#proposition span').innerHTML += tenteTaChance + ' ';
-				tentatives = tentatives - 1;
-				document.querySelector('#statut p span.tentatives').innerHTML = tentatives;
-				if (tentatives > 0)
-					{
-						switch (tentatives)
-						{
-							case 10:
-								document.querySelector('#statut div#pendu').innerHTML = "<img src='./pendu-image/pendu1.png' width='75%'/>";
-								break;
-							case 9:
-								document.querySelector('#statut div#pendu').innerHTML = "<img src='./pendu-image/pendu2.png' width='75%'/>";
-								break;
-							case 8:
-								document.querySelector('#statut div#pendu').innerHTML = "<img src='./pendu-image/pendu3.png' width='75%'/>";
-								break;
-							case 7:
-								document.querySelector('#statut div#pendu').innerHTML = "<img src='./pendu-image/pendu4.png' width='75%'/>";
-								break;
-							case 6:
-								document.querySelector('#statut div#pendu').innerHTML = "<img src='./pendu-image/pendu5.png' width='75%'/>";
-								break;
-							case 5:
-								document.querySelector('#statut div#pendu').innerHTML = "<img src='./pendu-image/pendu6.png' width='75%'/>";
-								break;
-							case 4:
-								document.querySelector('#statut div#pendu').innerHTML = "<img src='./pendu-image/pendu7.png' width='75%'/>";
-								break;
-							case 3:
-								document.querySelector('#statut div#pendu').innerHTML = "<img src='./pendu-image/pendu8.png' width='75%'/>";
-								break;
-							case 2:
-								document.querySelector('#statut div#pendu').innerHTML = "<img src='./pendu-image/pendu9.png' width='75%'/>";
-								break;
-							case 1:
-								document.querySelector('#statut div#pendu').innerHTML = "<img src='./pendu-image/pendu10.png' width='75%'/>";
-								break;
-						}
-					}
-
-				else if (tentatives <= 0)
-					{
-						document.querySelector('#statut div#pendu').innerHTML = "<img src='./pendu-image/pendu11.png' width='75%'/>";
-						alert('échec du jeu');
-						reinitialisation();
-					}
-			}
-		else if (positions.length >= 0)
-			{
-				for (i = 0 ; i < positions.length; i++)
-					{	//il faut utiliser la conversion unicode du chiffre pur l'utiliser dans le query selector  -> https://stackoverflow.com/questions/20306204/using-queryselector-with-ids-that-are-numbers
-						var cible = '#\\3' + positions[i];
-						document.querySelector('#mot-mystere ' +cible).innerText = tenteTaChance;
-					}
 
 			}
-
-
 	}
+
 
 deviner.addEventListener('click', function ()
 {
@@ -162,8 +206,10 @@ deviner.addEventListener('click', function ()
 
 
 /** TO DO :
- * limiter le champs de devinette à 1 lettre - bloquer les chiffres et convertir les accents, tout mettre en majuscule,
+ * bloquer accents et chiffres (ou faire tout l'alphabet en majusulet si pas dedans, alors ca bloque)
+ * convertir les accents( gérer les î et tout avec charcode ?),
  * prevoir alert fin du jeu + reinitialisation. (ou pas)-
+ * faire une bdd de mot ?
  *ajouter touchstart en envent en plus du clic ? (pour gestion tablette)
  * **/
 
